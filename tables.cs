00391b3df2087b5a05003d076b0c757dd29ca5e2
vladle <165798388+vadleshch@users.noreply.github.com>
2025-10-08 09:52:48 +0300

    Add files via upload

--- tables.cs
+++ tables.cs
@@ -8,10 +8,10 @@ namespace OAA_1
 {
     internal class tables
     {
-        string[,] table;
-        string name;
-        int[] indexed;
-        public tables(string tablename, string[] tableparam, int[] ifindexed) 
+        public string[,] table;
+        public string name;
+        //public bool[] indexed;
+        public tables(string tablename, string[] tableparam/*, bool[] ifindexed*/) 
         {
             table = new string[tableparam.Length, 1];
             for (int i = 0; i < tableparam.Length; i++)
@@ -19,8 +19,24 @@ namespace OAA_1
                 table[i, 0] = tableparam[i];
             }
             name = tablename;
-            indexed = ifindexed;
+            //indexed = ifindexed;
         }
-
+        public void InsertRow(string[] values)
+        {
+            if (values.Length != table.GetLength(0))
+                throw new ArgumentException("Забагато аргументів");
+            int oldCols = table.GetLength(0);
+            int oldRows = table.GetLength(1);
+            string[,] newTable = new string[oldCols, oldRows + 1];
+            for (int i = 0; i < oldCols; i++)
+            {
+                for (int j = 0; j < oldRows; j++)
+                {
+                    newTable[i, j] = table[i, j];
+                }
+                newTable[i, oldRows] = values[i];
+            }
+            table = newTable;
+        }
     }
 }
