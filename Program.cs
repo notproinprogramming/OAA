40ef9e15fb3db729477c122aba20749ae1208dd3
Mykyta Arshynets <122938560+notproinprogramming@users.noreply.github.com>
2025-10-08 10:14:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,18 @@
-﻿using OAA_1;
-using System;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
-using static System.Net.Mime.MediaTypeNames;
+﻿using System.Runtime.InteropServices;
 
 Console.WriteLine("Консольна програма");
 string str = "", s;
 char c = ' ';
-tables[] tables = new tables[0];
 while (true)
 {
     s = Console.ReadLine();
     if (s != "" && s[s.Length - 1] == ';')
     {
         int counter = s.Length - 2;
-        if (s.Length > counter)
+        while (s[counter] == ' ')
         {
-            while (counter > -1 &&  s[counter] == ' ')
-            {
-                s = s.Remove(counter, 1);
-                counter--;
-            }
+            s = s.Remove(counter, 1);
+            counter--;
         }
         s = ignorespace(s);
         if (s == "exit;")
@@ -28,21 +20,9 @@ while (true)
             break;
         }
         str += s;
-        if (str.Length > 1 && str[str.Length - 1] == ';' && str[str.Length - 2] == ' ')
-        {
-            str = str.Remove(str.Length - 2, 1);
-        }
         Console.WriteLine(str);
-        if (str == "exit;")
-        {
-            break;
-        }
-        SeekFunc(str);
+
         str = "";
-    }
-    else if (str == "" || str[str.Length - 1] != ' ')
-    {
-        str += ignorespace(s) + " ";
     }
     else
     {
@@ -59,16 +39,13 @@ string ignorespace(string str)
             str = str.Insert(i, " ");
         }
     }
-    if (str.Length > 0)
+    while (str[0] == ' ')
     {
-        while (str[0] == ' ')
-        {
-            str = str.Remove(0, 1);
-        }
-        while (str[str.Length - 1] == ' ')
-        {
-            str = str.Remove(str.Length - 1, 1);
-        }
+        str = str.Remove(0, 1);
+    }
+    while (str[str.Length - 1] == ' ')
+    {
+        str = str.Remove(str.Length - 1, 1);
     }
     for (int i = 0; i < str.Length - 1; i++)
     {
@@ -81,108 +58,15 @@ string ignorespace(string str)
         }
     }
     return str;
-}
-
-
+} 
 void SeekFunc(string str)
 {
-    //string[] command = str.Split(' ');
-    //if (command[command.Length - 1] == ";")
-    //{ 
-    //    string [] newcommand = new string[command.Length - 1];
-    //    for (int i = 0; i < command.Length - 1; i++)
-    //    {
-    //        newcommand[i] = command[i];
-    //    }
-    //    command = newcommand;
-    //    goto skip1;
-    //}
-    ////command[command.Length - 1] = command[command.Length - 1].Remove(command[command.Length - 1].Length - 1, 1);
-    //skip1:
-    string[] command = new string[0];
-    //try
-    //{
-        if (str.Split('(').Length == 2)
-        {
-            str = str.Remove(str.Length - 2, 2);
-            string[] semicommand = str.Split('(');
-        if (semicommand[1][0] == ' ')
-        {
-            semicommand[1].Remove(0, 1);
-        }
-        if (semicommand[0][semicommand[0].Length - 1] == ' ')
-        {
-            semicommand[0].Remove(semicommand[0].Length - 1, 1);
-        }
-    string[] com1 = semicommand[0].Split(' ');
-            string[] com2 = semicommand[1].Split(',');
-            for (int i = 0; i < com2.Length; i++)
-            {
-                for (int j = 0; j < com2[i].Length; j++)
-                {
-                    if (com2[i][j] == ' ')
-                    {
-                        com2[i] = com2[i].Remove(j, 1);
-                    }
-                }
-            }
-            command = new string[com1.Length + com2.Length];
-            for (int i = 0; i < com1.Length; i++)
-            {
-                command[i] = com1[i];
-            }
-            for (int i = 0; i < com2.Length; i++)
-            {
-                command[i + com1.Length] = com2[i];
-            }
-        }
-        else
-        {
-            if (str.Split('(').Length == 1)
-            {
-                str.Remove(str.Length - 1, 1);
-                command = str.Split(' ');
-            }
-            else
-            {
-                Console.WriteLine("Некоректна команда");
-                return;
-            }
-        }
-    //}
-    //catch
-    //{
-    //    Console.WriteLine("Некоректна команда");
-    //}
-    //try
-    //{ 
+    string[] command = str.Split(' ');
+    command[command.Length - 1] = command[command.Length - 1].Remove(s.Length - 1, 1);
     switch (command[0].ToLower())
     {
         case "create":
             {
-                tables[] newtables = new tables[tables.Length + 1];
-                for (int i = 0; i < tables.Length; i++)
-                {
-                    newtables[i] = tables[i];
-                }
-                    string[] param = new string[command.Length - 3];
-                for (int i = 3; i < command.Length; i++)
-                {
-                    param[i - 3] = command[i];
-                }
-                if (param[param.Length - 1][param[param.Length - 1].Length - 1] == ')')
-                    {
-                        param[param.Length - 1] = param[param.Length - 1].Remove(param[param.Length - 1].Length - 1, 1);
-                    }
-                newtables[newtables.Length - 1] = new tables(command[1], param);
-                tables = newtables;
-                Console.WriteLine("Таблицю створено успішно");
-                Console.WriteLine(tables[tables.Length - 1].name);
-                    for (int i = 0; i < tables[tables.Length - 1].table.GetLength(0); i++)
-                    {
-                        Console.WriteLine(tables[tables.Length - 1].table[i, 0]);
-                    }
-                    Console.WriteLine(param.Length);
                 break;
             }
         case "insert":
@@ -191,24 +75,7 @@ void SeekFunc(string str)
                 {
                     rem2comword(ref command);
                 }
-                int k = NameToIndex(command[1]);
-                    string[] data = new string[command.Length - 3];
-                    for (int i = 3; i < command.Length; i++)
-                    {
-                        data[i - 3] = command[i];
-                    data[i - 3] = data[i - 3].Remove(0, 1);
-                    data[i - 3] = data[i - 3].Remove(data[i - 3].Length - 1, 1);
-                    }
-                    if (k != -1)
-                    {
-                        tables[k].InsertRow(data);
-                        Console.WriteLine("Рядок додано");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Таблиці не існує");
-                    }
-                    break;
+                break;
             }
         case "select":
             {
@@ -218,33 +85,13 @@ void SeekFunc(string str)
                 }
                 break;
             }
-        case "print":
+        case "join":
             {
-                command[1] = command[1].Remove(command[1].Length - 1, 1);
-                if (NameToIndex(command[1]) != -1)
-                {
-                    int k = NameToIndex(command[1]);
-                    bool[] b = new bool[tables[k].table.GetLength(1)];
-                    for (int i = 0; i < b.Length; i++)
-                    {
-                        b[i] = true;
-                    }
-                    PrintTable(k, b);
-                }
-                else
-                {
-                    Console.WriteLine("Такої таблиці не існує");
-                }
                 break;
             }
         default:
             break;
     }
-    //} 
-    //    catch (Exception ex)
-    //    {
-    //        Console.WriteLine("Підчас виконання команди сталася помилка" + ex);
-    //    }
     void rem2comword(ref string[] command)
     {
         string[] newcommand = new string[command.Length - 1];
@@ -255,71 +102,4 @@ void SeekFunc(string str)
         }
         command = newcommand;
     }
-    int NameToIndex(string name)
-    {
-        Console.WriteLine("Шукаю таблицю за назваою: " + name);
-        for (int i = 0; i < tables.Length; i++)
-        {
-            if (tables[i].name == name)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    void PrintTable(int k, bool[] r)
-    {
-        
-    }
-    //string[,] table = tables[k].table;
-    //int len = table.GetLength(0);
-    //int rows = table.GetLength(1);
-    //int count = 0;
-    //for (int i = 0; i < rows; i++)
-    //{
-    //    if (i < r.Length && r[i])
-    //    {
-    //        count++;
-    //    }
-    //}
-    //int[] maxlen = new int[len];
-    //string[,] newtable = tables[k].table;
-    //for (int i = 0; i < len; i++)
-    //{
-    //    maxlen[i] = table[i, 0].Length;
-    //    for (int j = 1; j < len; j++)
-    //    {
-    //        if (j < r.Length && r[j])
-    //        {
-    //            if (table[i, j].Length > maxlen[i])
-    //            {
-    //                maxlen[i] = table[i, j].Length;
-    //            }
-    //            newtable[i, j] = table[i, j];
-    //        }
-    //    }
-    //}
-    //table = newtable;
-    //string section = "+";
-    //for (int i = 0; i < len; i++)
-    //{
-    //    for (int j = 0; j < maxlen[i] + 2; j++)
-    //    {
-    //        section += "-";
-    //    }
-    //    section += "+";
-    //}
-    //string str;
-    //for (int i = 0; i < table.GetLength(1); i++)
-    //{
-    //    str = "|";
-    //    Console.WriteLine(section);
-    //    for (int j = 0; j < table.GetLength(0); j++)
-    //    {
-    //        str += " " + table[i, j] + new string(' ', maxlen[i] - table[i, j].Length) + " |";
-    //    }
-    //    Console.WriteLine(str);
-
-    //}
-    //Console.WriteLine(section);
 }
--- tables.cs
+++ tables.cs
@@ -8,10 +8,11 @@ namespace OAA_1
 {
     internal class tables
     {
-        public string[,] table;
-        public string name;
-        //public bool[] indexed;
-        public tables(string tablename, string[] tableparam/*, bool[] ifindexed*/) 
+        private string[,] table;
+        private string name;
+        private int[] indexed;
+
+        public tables(string tablename, string[] tableparam, int[] ifindexed)
         {
             table = new string[tableparam.Length, 1];
             for (int i = 0; i < tableparam.Length; i++)
@@ -19,24 +20,172 @@ namespace OAA_1
                 table[i, 0] = tableparam[i];
             }
             name = tablename;
-            //indexed = ifindexed;
+            indexed = ifindexed;
         }
+
         public void InsertRow(string[] values)
         {
             if (values.Length != table.GetLength(0))
-                throw new ArgumentException("Забагато аргументів");
+                throw new ArgumentException("Кількість значень не відповідає кількості стовпців.");
+
             int oldCols = table.GetLength(0);
             int oldRows = table.GetLength(1);
+
             string[,] newTable = new string[oldCols, oldRows + 1];
             for (int i = 0; i < oldCols; i++)
             {
                 for (int j = 0; j < oldRows; j++)
-                {
                     newTable[i, j] = table[i, j];
-                }
+
                 newTable[i, oldRows] = values[i];
             }
+
             table = newTable;
+        }
+
+        public void SelectFrom(string command)
+        {
+            string[] parts = command.Substring("select ".Length).Split(' ', 2);
+            string targetTable = parts[0];
+
+            if (targetTable != name)
+            {
+                Console.WriteLine($" Таблиця '{targetTable}' не знайдена (очікувалась '{name}')");
+                return;
+            }
+            string rest = (parts.Length > 1) ? parts[1].Trim() : "";
+
+            string condition = null;
+            string order = null;
+
+            if (rest.Contains("WHERE"))
+            {
+                int whereIndex = rest.IndexOf("WHERE");
+                int orderIndex = rest.IndexOf("ORDER_BY");
+
+                if (orderIndex != -1)
+                {
+                    condition = rest.Substring(whereIndex + 5, orderIndex - (whereIndex + 5)).Trim();
+                    order = rest.Substring(orderIndex + 8).Trim().TrimEnd(';');
+                }
+                else
+                {
+                    condition = rest.Substring(whereIndex + 5).Trim().TrimEnd(';');
+                }
+            }
+            else if (rest.Contains("ORDER_BY"))
+            {
+                int orderIndex = rest.IndexOf("ORDER_BY");
+                order = rest.Substring(orderIndex + 8).Trim().TrimEnd(';');
+            }
+
+            int rows = table.GetLength(1);
+            int cols = table.GetLength(0);
+            List<string[]> data = new List<string[]>();
+            for (int r = 1; r < rows; r++)
+            {
+                string[] row = new string[cols];
+                for (int c = 0; c < cols; c++)
+                    row[c] = table[c, r];
+                data.Add(row);
+            }
+
+            if (condition != null)
+                data = ApplyWhere(data, condition);
+
+            if (order != null)
+                data = ApplyOrderBy(data, order);
+
+            PrintTable(data);
+        }
+
+        private List<string[]> ApplyWhere(List<string[]> data, string condition)
+        {
+            string[] ops = condition.Split('>');
+            if (ops.Length != 2)
+                return data;
+
+            string left = ops[0].Trim();
+            string right = ops[1].Trim().Trim('"');
+
+            int leftIndex = GetColumnIndex(left);
+            int rightIndex = GetColumnIndex(right);
+
+            if (leftIndex == -1)
+                return data;
+
+            List<string[]> filtered = new List<string[]>();
+
+            foreach (var row in data)
+            {
+                string leftVal = row[leftIndex];
+                string rightVal = rightIndex != -1 ? row[rightIndex] : right;
+                if (string.Compare(leftVal, rightVal, StringComparison.Ordinal) > 0)
+                    filtered.Add(row);
+            }
+
+            return filtered;
+        }
+
+        private List<string[]> ApplyOrderBy(List<string[]> data, string order)
+        {
+            string[] orderParts = order.Split(',');
+
+            // Зберігаємо список (стовпець, напрямок)
+            var orders = new List<(int index, bool asc)>();
+            foreach (string part in orderParts)
+            {
+                string[] temp = part.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string col = temp[0];
+                bool asc = (temp.Length < 2 || temp[1].ToUpper() == "ASC");
+                int idx = GetColumnIndex(col);
+                if (idx != -1)
+                    orders.Add((idx, asc));
+            }
+
+            // Каскадне сортування
+            data = data.OrderBy(row => 0).ToList(); // щоб створити IOrderedEnumerable
+            IOrderedEnumerable<string[]> sorted = null;
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                int idx = orders[i].index;
+                bool asc = orders[i].asc;
+                if (i == 0)
+                    sorted = asc
+                        ? data.OrderBy(r => r[idx], StringComparer.Ordinal)
+                        : data.OrderByDescending(r => r[idx], StringComparer.Ordinal);
+                else
+                    sorted = asc
+                        ? sorted.ThenBy(r => r[idx], StringComparer.Ordinal)
+                        : sorted.ThenByDescending(r => r[idx], StringComparer.Ordinal);
+            }
+
+            return sorted?.ToList() ?? data;
+        }
+
+        private int GetColumnIndex(string colName)
+        {
+            for (int i = 0; i < table.GetLength(0); i++)
+                if (table[i, 0] == colName)
+                    return i;
+            return -1;
+        }
+
+        private void PrintTable(List<string[]> data)
+        {
+            int cols = table.GetLength(0);
+            for (int i = 0; i < cols; i++)
+                Console.Write($"{table[i, 0],-12}");
+            Console.WriteLine();
+            Console.WriteLine(new string('-', cols * 12));
+
+            foreach (var row in data)
+            {
+                for (int i = 0; i < cols; i++)
+                    Console.Write($"{row[i],-12}");
+                Console.WriteLine();
+            }
         }
     }
 }
